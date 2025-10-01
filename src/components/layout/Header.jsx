76b186c85f29a0aa1eec636d7e0cb2aa2eac84d0
fredrawi <fredrikandreas03@gmail.com>
2025-10-01 12:06:40 +0200

    Finished the header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,9 +4,13 @@ import {Button} from "@/components/ui/button";
 
 export const Header = () => {
     return (
-        <main className="flex items-center justify-between border pl-sides pr-sides pt-5 pb-5 w-full h-[80px]">
-            <div className="flex items-center justify-center bg-primary h-full aspect-square text-white font-bold rounded-md">V</div>
-            <Nav />
+        <main className="fixed top-0 left-0 w-full h-[80px] px-sides py-5 flex items-center justify-between bg-white shadow z-50">
+            <div className="flex items-center justify-center bg-primary h-full aspect-square text-white font-bold rounded-md">
+                V
+            </div>
+
+            <div className="absolute left-1/2 transform -translate-x-1/2"><Nav/></div>
+
             <Button>Logg inn</Button>
         </main>
     )
