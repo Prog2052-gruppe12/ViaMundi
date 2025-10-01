ed59083d78c62b317a0b9c9c2282857de387b26f
fredrawi <fredrikandreas03@gmail.com>
2025-10-01 11:42:55 +0200

    Update header and button to match wireframe

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Nav} from "@/components/layout/Nav"
+import {Button} from "@/components/ui/button";
 
 export const Header = () => {
     return (
         <main className="flex items-center justify-between border pl-sides pr-sides pt-5 pb-5 w-full h-[80px]">
             <div className="flex items-center justify-center bg-primary h-full aspect-square text-white font-bold rounded-md">V</div>
             <Nav />
+            <Button>Logg inn</Button>
         </main>
     )
 }
