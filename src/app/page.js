889dadf280a1c60f5d8b2d377b5203ae1f1e5412
fredrawi <fredrikandreas03@gmail.com>
2025-10-08 16:05:14 +0200

    Finished form

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,19 @@ import {Header} from "@/components/layout/Header"
 import {Footer} from "@/components/layout/Footer"
 import {Section} from "@/components/common/Section"
 import {SearchForm} from "@/components/features/landing/SearchForm"
+import {Separator} from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-baseline font-sans min-h-screen h-fit bg-background pt-20">
+    <div className="min-h-screen bg-white">
         <Header />
-        <div className="flex flex-col items-center px-38 py-12 w-full h-fit min-h-screen gap-y-12">
+        <main className="flex flex-col items-center w-full h-fit min-h-screen mt-20 gap-y-12">
             <Section>
                 <h1 className="font-bold text-5xl text-center text-primary-foreground">Veien til din drømmereise</h1>
                 <SearchForm/>
             </Section>
-        </div>
+        </main>
         <Footer />
-    </main>
+    </div>
   );
 }
