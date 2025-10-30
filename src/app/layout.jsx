ff95b455d7a587b158fcff828c5b5535f7b488b7
Nahom <nahomberhane101@gmail.com>
2025-10-30 18:42:04 +0100

    fix syntax error . dobbel tick.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -31,7 +31,7 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
         <body
-            className={`${geistSans.variable} ${geistMono.variable} ${fontLogo.variable} antialiased flex flex-col min-h-screen\``}
+            className={`${geistSans.variable} ${geistMono.variable} ${fontLogo.variable} antialiased flex flex-col min-h-screen`}
         >
             <SidebarProvider defaultOpen={false}>
                 <SidebarNav />
